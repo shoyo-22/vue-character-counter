3f028b578f10e16816b6b3a6b56705b8187dc67e
shoyo <shoyo2209@gmail.com>
2025-03-20 15:48:20 +0500

    feat: added main heading text on root page

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { AppNavbar } from '@/widgets/navbar';
 import { useTheme } from '@/features/theme-switcher';
+import { BaseContainer } from '@/shared/ui';
 
 const { initTheme, provideTheme } = useTheme();
 
@@ -10,5 +11,13 @@ provideTheme();
 
 <template>
   <AppNavbar />
-  <main>Character Counter App</main>
+  <main>
+    <BaseContainer>
+      <h2
+        class="text-app-neutral-900 dark:text-app-neutral-100 my-12 text-center leading-none font-bold tracking-[-1px] transition-all duration-500 max-[990px]:my-10 max-sm:text-[40px] sm:text-[64px]"
+      >
+        Analyze your text in real-time.
+      </h2>
+    </BaseContainer>
+  </main>
 </template>
