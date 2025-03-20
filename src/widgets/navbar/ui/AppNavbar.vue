9a037907e5a68ae4378984cd9031c2c544de3af4
shoyo <shoyo2209@gmail.com>
2025-03-20 12:52:19 +0500

    refactor: handle dark mode for navbar title

--- src/widgets/navbar/ui/AppNavbar.vue
+++ src/widgets/navbar/ui/AppNavbar.vue
@@ -8,7 +8,7 @@ import { ThemeSwitcher } from '@/features/theme-switcher';
     <BaseContainer class="flex py-4">
       <div class="flex flex-1">
         <span
-          class="leading-[1.3] font-semibold max-sm:text-lg max-sm:tracking-[-0.75px] sm:text-2xl sm:tracking-[-1px]"
+          class="dark:text-app-neutral-200 text-app-neutral-900 leading-[1.3] font-semibold transition-colors duration-300 max-sm:text-lg max-sm:tracking-[-0.75px] sm:text-2xl sm:tracking-[-1px]"
         >
           Character Counter
         </span>
